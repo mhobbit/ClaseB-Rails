420321e86390d4d7b83f4cec9b0a31b64b1754c5
Nicolas Aravena <mhobbit@gmail.com>
2015-12-02 18:44:45 +0000

    asdsad

--- app/controllers/estadisticas_controller.rb
+++ app/controllers/estadisticas_controller.rb
@@ -1,4 +1,5 @@
 class EstadisticasController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_estadistica, only: [:show, :edit, :update, :destroy]
 
   # GET /estadisticas
